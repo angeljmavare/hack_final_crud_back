4e99490684f8f8377283cc3b2ff1d69d30648518
AngelJMavare <angeljmavare@gmail.com>
2025-05-22 16:31:37 -0400

    Update: index.js

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+const cors = require('cors');
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+const db = new Pool({
+    host: 'db-crud-usuarios-final.cj2cywk4i2hh.us-east-1.rds.amazonaws.com',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'db_crud_usuarios_final',
+    port: 5432,
+     ssl: { // Habilitar SSL para que se establezca la conexión segura con la base de datos
+         rejectUnauthorized: false
+       }
+  });
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      return;
+    }
+    console.log('Conexión a la base de datos establecida');
+  });  
+
+app.post('/create', (req, res) => {
+    const { nombre, edad, correo } = req.body;
+    db.query('INSERT INTO usuarios (nombre, edad, correo) VALUES ($1, $2, $3)', [nombre, edad, correo],
+        (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+app.get('/usuarios', (req, res) => {
+    db.query('SELECT * FROM usuarios', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result.rows);
+        }
+    });
+});
+
+app.put('/update', (req, res) => {
+    const { id, nombre, edad, correo } = req.body;
+
+    db.query('UPDATE usuarios SET nombre = $2, edad = $3, correo = $4 WHERE id = $1', [id, nombre, edad, correo],
+        (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.query('DELETE FROM usuarios WHERE id = $1', [id],
+        (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
